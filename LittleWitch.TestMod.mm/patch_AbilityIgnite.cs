4a83e9213009198cd879d4ad5f2b2dbfc223f7ef
stupid cat <cat@blargbot.xyz>
2020-10-29 10:29:30 -0600

    initial commit

---
+++ LittleWitch.TestMod.mm/patch_AbilityIgnite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameEngine;
+
+namespace LittleWitch
+{
+    public class patch_AbilityIgnite : AbilityIgnite
+    {
+        public override bool InRange(BattleGrid grid, BattleUnit unit, VectorI target)
+        {
+            return true;
+        }
+    }
+}
